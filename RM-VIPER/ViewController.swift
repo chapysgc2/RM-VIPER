e226a0d34f1a4c475f3b9057091040f18f23b5e5
chapysgc2 <hazelo235@gmail.com>
2024-04-23 16:56:14 -0600

    Initial Commit

---
+++ RM-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RM-VIPER
+//
+//  Created by Hazel Alain on 23/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
