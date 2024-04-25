65425a9df576ef0fea1c1e9b4a37cfe96e1d1cc0
chapysgc2 <hazelo235@gmail.com>
2024-04-25 15:19:54 -0600

    🎉presenter terminado

--- RM-VIPER/ListCharacter/Presenter/ListCharacterPresenter.swift
+++ RM-VIPER/ListCharacter/Presenter/ListCharacterPresenter.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol ListCharacterUI : AnyObject {
+    
+    func update(characters :  [ListCharacterEntity])
+}
+
+
+class ListCharacterPresenter {
+    
+    // Referencia débil a la vista que implementa el protocolo listCharacterUI
+    var ui : ListCharacterUI?
+    
+    // Instancia del interactor que se utilizará para obtener la lista de personajes
+    private var listCharacterInteractor = ListCharacterInteractor()
+    
+    init(listCharacterInteractor : ListCharacterInteractor) {
+        self.listCharacterInteractor = listCharacterInteractor
+    
+    }
+    
+    func onViewAppear() {
+           listCharacterInteractor.fetchCharacters { [weak self] characters, error in
+               if let error = error {
+                   print("Error fetching characters: \(error)")
+               } else if let characters = characters {
+                   self?.ui?.update(characters: characters)
+               }
+           }
+       }
+}
